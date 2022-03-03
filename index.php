ed01cde3fdded50a2bbc7401cab2139ffb960289
GTPE Student <lab-user@studentvm.sv28w.internal>
2022-03-03 09:19:32 +0000

    chnages

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
 	<h1>PHP Test</h1>
 		<p><b>An Gobinath Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOEDEMIDDAG GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
-	</body> 
+	echo date("g:i A l, F j Y.");?> </p>
+</body>
 </html>
